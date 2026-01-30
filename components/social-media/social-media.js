bebe3d63641394dc9ea712e5a1bbd5f23a3b637f
trump.liu <trump.liu@live.com>
2026-01-30 14:41:46 +0800

    up

--- components/social-media/social-media.js
+++ components/social-media/social-media.js
@@ -3,6 +3,7 @@ Component({
     showModal: false,
     modalTitle: '',
     qrcodeUrl: '',
+    showEmailText: false,
     email: 'engagementboy@163.com'
   },
 
@@ -13,7 +14,8 @@ Component({
       this.setData({
         showModal: true,
         modalTitle: title,
-        qrcodeUrl: qrcode
+        qrcodeUrl: qrcode,
+        showEmailText: false
       });
     },
 
@@ -27,6 +29,16 @@ Component({
     // 阻止冒泡
     preventClose() {
       // 空函数，用于阻止点击弹窗内容时关闭
+    },
+
+    // 显示邮箱
+    showEmail() {
+      this.setData({
+        showModal: true,
+        modalTitle: '邮箱地址',
+        qrcodeUrl: '', // 不显示二维码
+        showEmailText: true
+      });
     },
 
     // 复制邮箱
